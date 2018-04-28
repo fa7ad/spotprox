ff50e7bf0008f1f5343cd841b6fb88d67b518f72
Fahad Hossain <evilinfinite7@gmail.com>
2018-04-28 11:47:53 +0600

    make app cross-env

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
 #!/usr/bin/env node
 const fs = require('fs')
+const opn = require('opn')
 const path = require('path')
 const $ = require('shelljs')
-const cp = require('child_process')
 const _sample = require('lodash/sample')
 const proxyFetcher = require('proxy-lists')
 
 const proxyOptions = {
   countries: ['us'],
-  protocols: ['http', 'https'],
+  protocols: ['http'],
   ipTypes: ['ipv4'],
-  sourcesBlackList: ['bitproxies', 'kingproxies', 'blackhatworld']
+  sourcesBlackList: ['bitproxies', 'kingproxies']
 }
 let proxList = []
 
@@ -18,24 +18,31 @@ const fetchProxy = proxyFetcher.getProxies(proxyOptions)
 
 console.log('Downloading proxy list...')
 fetchProxy.on('data', function (proxies) {
-  process.stdout.clearLine()
-  process.stdout.cursorTo(0)
-
-  if (proxList.length > 150) return
-
-  proxList = proxList.concat(proxies.map(p => `${p.ipAddress}:${p.port}`))
-  process.stdout.write('...')
+  if (proxList.length > 50) return this.emit('end')
+  proxList.push(...proxies.map(p => `${p.ipAddress}:${p.port}`))
+  process.stdout.write('*')
 })
 
-fetchProxy.on('error', function (err) {
-  process.stderr.write(err.toString())
-  process.stdout.cursorTo(0)
-  process.stdout.write('*')
+fetchProxy.on('error', function () {
+  process.stdout.write('x')
+  if (proxList.length > 50) this.emit('end')
 })
 
 fetchProxy.once('end', function () {
   console.log('DONE!')
-  const config = path.join(process.env.HOME, '.config', 'spotify')
+  let config
+  if (process.platform === 'win32') {
+    config = path.join(process.env.APPDATA, 'spotify')
+  } else if (process.platform === 'darwin') {
+    config = path.join(
+      process.env.USER,
+      'Library',
+      'Application Support',
+      'Spotify'
+    )
+  } else {
+    config = path.join(process.env.HOME, '.config', 'spotify')
+  }
   const prefsFile = path.join(config, 'prefs')
 
   if (!fs.existsSync(prefsFile)) {
@@ -44,7 +51,7 @@ fetchProxy.once('end', function () {
   }
   const prefs = fs.readFileSync(prefsFile, 'utf8')
 
-  if (prefs.search('network.proxy.mode') >= 0) {
+  if (prefs.search('network.proxy.mode') > -1) {
     $.sed('-i', /^network.proxy.mode.*$/, 'network.proxy.mode=2', prefsFile)
   } else {
     fs.appendFileSync(prefsFile, 'network.proxy.mode=2')
@@ -63,8 +70,8 @@ fetchProxy.once('end', function () {
   }
 
   process.stdout.write('Launching spotify...')
-  cp.execFile('spotify', function () {
-    console.log('Closed Spotify.')
-    process.exit()
-  })
+
+  opn('', {
+    app: 'spotify'
+  }).then(process.exit)
 })
